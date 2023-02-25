2a5b267a45c88f326102aec90c696afc4b2667c4
BearonCoke <63679988+BearonCoke@users.noreply.github.com>
2023-02-24 23:21:40 -0600

    Changed to animated sprite
    added walk cycle

--- objects/Player.gd
+++ objects/Player.gd
@@ -24,14 +24,22 @@ func add_drag(speed, decrementer):
 		return speed+decrementer
 
 func _physics_process(delta):
+	var play_walk = true
+	if abs(speed_x) < 10:
+		$AnimatedSprite.play("default")
 	if not is_on_floor():
 		speed_y += weight*delta
+		play_walk = false
 	else:
 		if Input.is_action_pressed("ui_up"):
 			speed_y = -200
 	if Input.is_action_pressed("ui_right"):
+		if play_walk == true:
+			$AnimatedSprite.play("walk")
 		speed_x = 200
 	if Input.is_action_pressed("ui_left"):
+		if play_walk == true:
+			$AnimatedSprite.play("walk")
 		speed_x = -200
 	if not is_on_floor():
 		speed_x = add_drag(speed_x,drag*delta)
@@ -39,6 +47,7 @@ func _physics_process(delta):
 		speed_x = add_drag(speed_x,friction*delta)
 	var velocity = Vector2(speed_x, speed_y)
 	move_and_slide(velocity, Vector2.UP)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass

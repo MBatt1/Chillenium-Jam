f3bdd5fef4f77f227e4d04dc73d469d371d556c4
BearonCoke <63679988+BearonCoke@users.noreply.github.com>
2023-02-25 14:38:30 -0600

    Switches

--- level/Editor/switches/lever.gd
+++ level/Editor/switches/lever.gd
@@ -4,18 +4,24 @@ extends StaticBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var activate = false
-
+export(NodePath) var connection
+var on = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
+func switch_state():
+	if $AnimatedSprite.animation == "off":
+		$AnimatedSprite.play("on")
+		on = true
+	else:
+		$AnimatedSprite.play("off")
+		on = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if activate == true:
-		$killbox/CollisionShape2D.disabled = true
+#func _process(delta):
+#	pass
 
-func _on_killbox_body_entered(body):
-	body.kill_player()
+func _on_Area2D_body_entered(body):
+	switch_state()
+	get_node(connection).activate = true
 	pass # Replace with function body.

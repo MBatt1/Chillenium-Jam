fc888ff0e06c75f065e569f59ec2260faf3fd037
Maxx <53229958+MBatt1@users.noreply.github.com>
2023-02-25 15:54:10 -0600

    simple homing heart

--- level/LevelScript.gd
+++ level/LevelScript.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+export(int) var heart_start
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- objects/Player.gd
+++ objects/Player.gd
@@ -101,3 +101,4 @@ func _process(delta):
 
 
 
+

3c09f8e9229f1e452c4ffd92ed1f4cd0b28eebb1
BearonCoke <63679988+BearonCoke@users.noreply.github.com>
2023-02-25 23:04:58 -0600

    butt pound

--- level/Editor/switches/pressure_plate.gd
+++ level/Editor/switches/pressure_plate.gd
@@ -4,7 +4,7 @@ extends StaticBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+export(NodePath) var connection
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,5 +16,9 @@ func _ready():
 #	pass
 
 func _on_Area2D_body_entered(body):
-	if (body.name == 'player' or body.name == '@player@2') and body.speed >400:
-		print('hi')
+	if (body.name == 'player' or body.name == '@player@2') and body.speed_y >= 300:
+		get_node(connection).activate = true
+
+func _on_Area2D_body_exited(body):
+	if (body.name == 'player' or body.name == '@player@2'):
+		get_node(connection).activate = false
